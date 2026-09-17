66b3562482daf8a14a7c5f304f31124a97bffbdf
shadabhashmi21 <er.shadabhashmi@gmail.com>
2026-09-17 20:11:46 +0530

    added: safe area and appbar

--- shared/src/commonMain/kotlin/com/example/movieexplorer/presentation/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/com/example/movieexplorer/presentation/home/HomeScreen.kt
@@ -1,13 +1,22 @@
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -17,30 +26,51 @@ fun HomeScreen(
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.safeDrawing)
     ) {
+        Text(
+            text = "Movie Explorer",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(
+                all = 16.dp,
+            )
+        )
+
         when (val state = uiState) {
             is HomeUiState.Loading -> {
-                CircularProgressIndicator()
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
             }
 
             is HomeUiState.Success -> {
-                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(all = 8.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
                     items(
                         items = state.movies,
-                        key = {it.id}
-                    ){
-                        movie -> MovieCard(movie = movie)
+                        key = { it.id }
+                    ) { movie ->
+                        MovieCard(movie = movie)
                     }
                 }
             }
 
             is HomeUiState.Error -> {
-                Text(
-                    text = state.message
-                )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = state.message
+                    )
+                }
             }
         }
     }
